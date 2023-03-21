6c80e3dbbd2632f88ba853c715b178c9d6a5989c
AndyG1228 <606925@student.dusd.net>
2023-03-21 11:41:46 -0700

    Completed Main Unit 3 Tutorial
    Began work on bonus feature 1.

--- SoundAndEffectsGallardoP3/Assets/Scripts/SpawnManager.cs
+++ SoundAndEffectsGallardoP3/Assets/Scripts/SpawnManager.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private int randomObstacle;
+
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript =
+        GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
+        }
     }
 }
