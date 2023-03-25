673f83f78406830f5ce2c12dc4dec5b6c8596e6e
AndyG1228 <606925@student.dusd.net>
2023-03-24 20:22:50 -0700

    Revert "Finished Dash Mode"
    This reverts commit 1b149d2c850d8e2ba7d66a48caf45a92f5af1e38.

--- SoundAndEffectsGallardoP3/Assets/Scripts/MoveLeft.cs
+++ SoundAndEffectsGallardoP3/Assets/Scripts/MoveLeft.cs
@@ -20,16 +20,8 @@ public class MoveLeft : MonoBehaviour
     {
         if (playerControllerScript.gameOver == false)
         {
-            if (playerControllerScript.doubleSpeed)
-            {
-                transform.Translate(Vector3.left * Time.deltaTime * (speed * 2));
-            }
-            else
-            {
-                transform.Translate(Vector3.left * Time.deltaTime * speed);
-            }
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
-        
         if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
         {
             Destroy(gameObject);
